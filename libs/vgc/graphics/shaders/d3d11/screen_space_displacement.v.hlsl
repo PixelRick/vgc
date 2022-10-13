1d3808d4e3d6ccfe8b0432fef978d69b546dfb39
Even ENTEM <even.entem@gmail.com>
2022-10-13 20:20:23 +0200

    wip (todo:check duplicate point bug)

--- libs/vgc/graphics/shaders/d3d11/screen_space_displacement.v.hlsl
+++ libs/vgc/graphics/shaders/d3d11/screen_space_displacement.v.hlsl
@@ -29,17 +29,21 @@ PS_INPUT main(VS_INPUT input) {
     float2 dispDir = normalize(viewDisp).xy;
     float dispMag = length(input.disp);
 
-    float p = (1.0 + cos((float(frameStartTimeInMs) + input.vid) / 300.0f)) * 0.5;
-    float animScale = 1.0 + 2.0 * (1.0 + cos((float(frameStartTimeInMs) + input.vid) / 300.0f));
+    float p = (1.0f + cos((float(frameStartTimeInMs) - input.vid) / 300.0f)) * 0.5f;
+    float p2 = (1.0f + cos((float(frameStartTimeInMs) - input.vid + 150.f) / 320.0f)) * 0.5f;
+    float animScale = 1.0f + 4.0 * p;
     if (input.iid > 0) {
-        animScale = 1.0;
+        animScale = 1.0f;
     }
 
     output.pos.xy += animScale * dispMag * dispDir / float2(0.5 * viewport.z, -0.5 * viewport.w);
     output.clipPos = output.pos;
     output.col = input.col;
 
-    output.col *= 0.5 + 0.5 * p;
+    output.col.r = p;
+    output.col.g = p2;
+    output.col.b = 1.0f - p2;
+    output.col.a *= 0.1f + 0.9f * p;
 
     return output;
 }
